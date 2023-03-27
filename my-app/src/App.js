53ce4d9268894d91760b589054ec80ed900fc498
Higgs32584 <mjhiggins2@loyola.edu>
2023-03-27 15:30:55 -0400

    front end page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function Login({ onLogin }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onLogin();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form className="login-form" onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={handleEmailChange} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <h1>Dashboard</h1>
+      {/* display user's progress and goals here */}
+    </div>
+  );
+}
+
+function GoalForm() {
+  const [goal, setGoal] = useState('');
+  const [progress, setProgress] = useState(0);
+
+  function handleGoalChange(event) {
+    setGoal(event.target.value);
+  }
+
+  function handleProgressChange(event) {
+    setProgress(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // handle goal and progress logic here
+  }
+
+  return (
+    <form className="goal-form" onSubmit={handleSubmit}>
+      <h2>Set Goal and Record Progress</h2>
+      <label>
+        Goal:
+        <input type="text" value={goal} onChange={handleGoalChange} />
+      </label>
+      <label>
+        Progress:
+        <input type="number" value={progress} onChange={handleProgressChange} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  function handleLogin() {
+    setIsLoggedIn(true);
+  }
+
+  function handleLogout() {
+    setIsLoggedIn(false);
+  }
+
+  return (
+    <div className="app-container">
+      {isLoggedIn ? (
+        <div className="logged-in-container">
+          <button className="logout-button" onClick={handleLogout}>Logout</button>
+          <Dashboard />
+          <GoalForm />
+        </div>
+      ) : (
+        <Login onLogin={handleLogin} />
+      )}
     </div>
   );
 }
