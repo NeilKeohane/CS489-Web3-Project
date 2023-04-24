b38da15453524fc3f185144ad20765c9fab67b0e
Higgs32584 <mjhiggins2@loyola.edu>
2023-04-24 13:13:50 -0400

    keep all solidity files the same version

--- April18/Solidity/contracts/Workout.sol
+++ April18/Solidity/contracts/Workout.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.19;
 
 import "hardhat/console.sol";
 import "../../../contracts/MilestonesCoin.sol";
--- contracts/LoyolaCS.sol
+++ contracts/LoyolaCS.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 import "hardhat/console.sol";
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.19;
 
 contract LoyolaCS {
         struct User
